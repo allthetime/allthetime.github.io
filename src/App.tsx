921e64d9998de7e2ce0333e712cf4afec0c5a127
Christopher Reid <chris@mbp.local>
2020-11-19 22:44:58 -0800

    d

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import './App.css';
 import { about, at_post } from './data.json'
 import ReactMarkdown from 'react-markdown'
 
-const empty: string[] = [];
+type postObj = {
+    md: string;
+    filename: string;
+}
+type posts = postObj[];
+
+const empty: posts = [];
 
 function App() {
 
@@ -11,13 +17,16 @@ function App() {
 
   function getPosts() {
     (async ()=> {
-        let posts_:string[] = [];
+        let posts_:posts = [];
         for (let i=1;i<=at_post;i++) {
             const filename = `00${i}`.substr(-3);
             const md = require(`./posts/${filename}.md`).default;
             const md_r = await fetch(md);
             const md_t: string = await md_r.text();
-            posts_.push(md_t)
+            posts_.push({
+                md: md_t,
+                filename
+            })
         }
         posts_.reverse()
         setPosts(posts_)
@@ -30,11 +39,12 @@ function App() {
     <div className="App">
         <span className="aboutText">{ about }</span>
         <div className="Posts">{
-            posts && posts.map(p=>{
+            posts && posts.map((p,index)=>{
                 return (
                     <div className="Post">
+                        <span className="fileName">{p.filename}</span>
                         <div className="PostWrapper">
-                            <ReactMarkdown children={p}/>
+                            <ReactMarkdown children={p.md}/>
                         </div>
                     </div>
                 )
